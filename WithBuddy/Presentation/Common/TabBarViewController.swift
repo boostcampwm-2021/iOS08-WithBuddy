a5dba3a34f1558dde675dec6ba0a2f8b73edb288
co3oing <co3oing@naver.com>
2021-11-01 18:37:49 +0900

    (#3) feat: ViewController 추가

--- WithBuddy/Presentation/Common/TabBarViewController.swift
+++ WithBuddy/Presentation/Common/TabBarViewController.swift
@@ -12,11 +12,11 @@ final class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(named: "BackgroundPurple")
-        let calendar = configureTab(controller: RegisterViewController(), title: "일정", photoName: "calendar")
-        let chart = configureTab(controller: RegisterViewController(), title: "통계", photoName: "chart.bar.xaxis")
+        let calendar = configureTab(controller: CalendarViewController(), title: "일정", photoName: "calendar")
+        let chart = configureTab(controller: ChartViewController(), title: "통계", photoName: "chart.bar.xaxis")
         let register = configureTab(controller: RegisterViewController(), title: "모임등록", photoName: "person.3.fill")
-        let list = configureTab(controller: RegisterViewController(), title: "목록", photoName: "list.bullet.rectangle.fill")
-        let setting = configureTab(controller: RegisterViewController(), title: "설정", photoName: "gearshape.fill")
+        let list = configureTab(controller: ListViewController(), title: "목록", photoName: "list.bullet.rectangle.fill")
+        let setting = configureTab(controller: SettingViewController(), title: "설정", photoName: "gearshape.fill")
         self.viewControllers = [calendar, chart, register, list, setting]
     }
     

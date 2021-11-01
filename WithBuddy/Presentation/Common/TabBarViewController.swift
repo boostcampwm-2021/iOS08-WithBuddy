94d88a6d4788c7db11a3d9539d97a1169bda2781
co3oing <co3oing@naver.com>
2021-11-01 23:23:34 +0900

    (#3) feat: 모임등록 pushViewController 구현

--- WithBuddy/Presentation/Common/TabBarViewController.swift
+++ WithBuddy/Presentation/Common/TabBarViewController.swift
@@ -9,15 +9,19 @@ import UIKit
 
 final class TabBarViewController: UITabBarController {
     
+    private var prevIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(named: "BackgroundPurple")
+        self.delegate = self
         let calendar = configureTab(controller: CalendarViewController(), title: "일정", photoName: "calendar")
         let chart = configureTab(controller: ChartViewController(), title: "통계", photoName: "chart.bar.xaxis")
         let register = configureTab(controller: RegisterViewController(), title: "모임등록", photoName: "person.3.fill")
         let list = configureTab(controller: ListViewController(), title: "목록", photoName: "list.bullet.rectangle.fill")
         let setting = configureTab(controller: SettingViewController(), title: "설정", photoName: "gearshape.fill")
         self.viewControllers = [calendar, chart, register, list, setting]
+        self.tabBarController?.tabBar.items?[2].isEnabled = false
     }
     
     private func configureTab(controller: UIViewController, title: String, photoName: String) -> UINavigationController {
@@ -26,5 +30,19 @@ final class TabBarViewController: UITabBarController {
         tab.tabBarItem = tabBarItem
         return tab
     }
+    
+}
 
+extension TabBarViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        let index = tabBarController.selectedIndex
+        if index == 2 {
+            self.navigationController?.pushViewController(RegisterViewController(), animated: true)
+            tabBarController.selectedIndex = prevIndex
+        } else {
+            self.prevIndex = index
+        }
+    }
+    
 }
--- WithBuddy/Presentation/Register/RegisterViewController.swift
+++ WithBuddy/Presentation/Register/RegisterViewController.swift
@@ -11,7 +11,8 @@ class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "모임등록"
+        self.view.backgroundColor = UIColor(named: "BackgroundPurple")
     }
 
 }

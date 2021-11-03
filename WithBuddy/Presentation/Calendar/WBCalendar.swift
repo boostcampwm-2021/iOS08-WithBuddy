b8e82329edaec6de3db9862271ef915f15d88f30
dailynj <leena9901@gmail.com>
2021-11-03 15:34:27 +0900

    (#12) 달력 상단의 이번달 & 이전/다음 달 이동버튼 추가

--- WithBuddy/Presentation/Calendar/WBCalendar.swift
+++ WithBuddy/Presentation/Calendar/WBCalendar.swift
@@ -8,26 +8,42 @@
 import UIKit
 
 class WBCalendar: UIView {
-    private let testView = UIView()
+    private let thisMonth = UILabel()
+    private let prevMonthButton = UIButton()
+    private let nextMonthButton = UIButton()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.configurtestView()
+        self.configureMonth()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.configurtestView()
+        self.configureMonth()
     }
     
-    private func configurtestView() {
-        self.addSubview(testView)
-        self.testView.translatesAutoresizingMaskIntoConstraints = false
+    private func configureMonth() {
+        self.addSubview(thisMonth)
+        self.addSubview(prevMonthButton)
+        self.addSubview(nextMonthButton)
+        thisMonth.text = "2021년 10월"
+        thisMonth.textColor = UIColor(named: "LabelPurple")
+        thisMonth.font = .boldSystemFont(ofSize: 17)
+        prevMonthButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        nextMonthButton.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        self.thisMonth.translatesAutoresizingMaskIntoConstraints = false
+        self.prevMonthButton.translatesAutoresizingMaskIntoConstraints = false
+        self.nextMonthButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            self.testView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            self.testView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            self.testView.topAnchor.constraint(equalTo: self.topAnchor),
-            self.testView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            self.thisMonth.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+            self.thisMonth.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.thisMonth.widthAnchor.constraint(equalToConstant: thisMonth.intrinsicContentSize.width),
+            self.prevMonthButton.centerYAnchor.constraint(equalTo: self.thisMonth.centerYAnchor),
+            self.nextMonthButton.centerYAnchor.constraint(equalTo: self.thisMonth.centerYAnchor),
+            self.prevMonthButton.trailingAnchor.constraint(equalTo: self.thisMonth.leadingAnchor),
+            self.prevMonthButton.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.nextMonthButton.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.nextMonthButton.leadingAnchor.constraint(equalTo: self.thisMonth.trailingAnchor)
         ])
     }
 }

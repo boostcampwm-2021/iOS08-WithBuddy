b280bc36c18b085738e62e871d5a1d843e58ca98
co3oing <co3oing@naver.com>
2021-11-11 16:17:11 +0900

    (#44) feat: 통계 화면 만남 목적 뷰 구현

--- WithBuddy/Presentation/Chart/PurposeChartView.swift
+++ WithBuddy/Presentation/Chart/PurposeChartView.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 final class PurposeChartView: UIView {
+    
+    private let nameLabel = NameLabel()
+    private let titleLabel = TitleLabel()
+    private let whiteView = WhiteView()
+    private let stackView = UIStackView()
+    private let firstPurposeView = PurposeView()
+    private let secondPurposeView = PurposeView()
+    private let thirdPurposeView = PurposeView()
+    private let fourthPurposeView = PurposeView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,7 +29,62 @@ final class PurposeChartView: UIView {
     }
     
     private func configure() {
-        self.backgroundColor = .systemPurple
+        self.configureNameLabel()
+        self.configureWhiteView()
+        self.configureTitleLabel()
+        self.configureStackView()
+        self.configurePurposeView()
+    }
+    
+    private func configureNameLabel() {
+        self.addSubview(self.nameLabel)
+        self.nameLabel.text = "위드버디"
+        self.nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.nameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.nameLabel.topAnchor.constraint(equalTo: self.topAnchor)
+        ])
+    }
+    
+    private func configureTitleLabel() {
+        self.addSubview(self.titleLabel)
+        self.titleLabel.text = "님의 만남 목적"
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.titleLabel.leadingAnchor.constraint(equalTo: self.nameLabel.trailingAnchor),
+            self.titleLabel.centerYAnchor.constraint(equalTo: self.nameLabel.centerYAnchor)
+        ])
+    }
+    
+    private func configureWhiteView() {
+        self.addSubview(self.whiteView)
+        self.whiteView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.whiteView.leadingAnchor.constraint(equalTo: self.nameLabel.leadingAnchor),
+            self.whiteView.topAnchor.constraint(equalTo: self.nameLabel.bottomAnchor, constant: 10),
+            self.whiteView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.whiteView.heightAnchor.constraint(equalToConstant: 100),
+            self.whiteView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    private func configureStackView() {
+        self.whiteView.addSubview(self.stackView)
+        self.stackView.spacing = 10
+        self.stackView.alignment = .center
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.stackView.centerXAnchor.constraint(equalTo: self.whiteView.centerXAnchor),
+            self.stackView.centerYAnchor.constraint(equalTo: self.whiteView.centerYAnchor),
+            self.stackView.heightAnchor.constraint(equalTo: self.whiteView.heightAnchor)
+        ])
+    }
+    
+    private func configurePurposeView() {
+        self.stackView.addArrangedSubview(self.firstPurposeView)
+        self.stackView.addArrangedSubview(self.secondPurposeView)
+        self.stackView.addArrangedSubview(self.thirdPurposeView)
+        self.stackView.addArrangedSubview(self.fourthPurposeView)
     }
 
 }
--- WithBuddy/Presentation/Chart/PurposeView.swift
+++ WithBuddy/Presentation/Chart/PurposeView.swift
@@ -7,14 +7,51 @@
 
 import UIKit
 
-class PurposeView: UIView {
+final class PurposeView: UIView {
+    
+    private let imageView = UIImageView()
+    private let label = UILabel()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configure()
+    }
+    
+    private func configure() {
+        self.configureImageView()
+        self.configureLabel()
+    }
+    
+    private func configureImageView() {
+        self.addSubview(self.imageView)
+        self.imageView.image = UIImage(systemName: "photo")
+        self.imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.imageView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.imageView.widthAnchor.constraint(equalToConstant: 65),
+            self.imageView.heightAnchor.constraint(equalToConstant: 65)
+        ])
+    }
+    
+    private func configureLabel() {
+        self.addSubview(self.label)
+        self.label.text = "버디"
+        self.label.font = .systemFont(ofSize: 12)
+        self.label.textAlignment = .center
+        self.label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.label.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.label.topAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: 4),
+            self.label.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.label.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
 
 }

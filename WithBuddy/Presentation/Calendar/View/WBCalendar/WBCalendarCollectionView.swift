bb1e4cf3c89689a69ed352031a4eeeb6fb4adc7f
duyeonnn <dykim0828@yonsei.ac.kr>
2021-11-14 23:33:09 +0900

    (#86) feat: CalendarCollectionView 구현

---
+++ WithBuddy/Presentation/Calendar/View/WBCalendar/WBCalendarCollectionView.swift
@@ -0,0 +1,42 @@
+//
+//  WBCalendarCollectionView.swift
+//  WithBuddy
+//
+//  Created by 김두연 on 2021/11/14.
+//
+
+import UIKit
+
+class WBCalendarCollectionView: UICollectionView {
+
+    init() {
+        super.init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configure()
+    }
+    
+    private func configure() {
+        self.register(WBCalendarViewCell.self, forCellWithReuseIdentifier: WBCalendarViewCell.identifier)
+        self.configureLayout()
+    }
+    
+    private func configureLayout() {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0/7.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0/6.0)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        
+        self.collectionViewLayout = UICollectionViewCompositionalLayout(section: section)
+    }
+}

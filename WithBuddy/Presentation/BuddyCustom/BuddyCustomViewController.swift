b5ad16b060fef69850c7a8543af5844a0bd869fe
inu <inwoo7233@naver.com>
2021-11-17 14:18:21 +0900

    (#90) feat: 캐릭터선택화면 View 구현세팅

--- WithBuddy/Presentation/BuddyCustom/BuddyCustomViewController.swift
+++ WithBuddy/Presentation/BuddyCustom/BuddyCustomViewController.swift
@@ -9,22 +9,94 @@ import UIKit
 
 class BuddyCustomViewController: UIViewController {
     
-    private var tempLabel = UILabel()
+    private var scrollView = UIScrollView()
+    private var contentView = UIView()
+    
+    private var nameTextField = UITextField()
+    private var lineView = UIView()
+    private var myBuddyImageView = UIImageView()
+    
+    private var colorTitleLabel = TitleLabel()
+    private var colorCollectionView = UICollectionView()
+    
+    private var faceTitleLabel = TitleLabel()
+    private var faceCollectionView = UICollectionView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        self.view.backgroundColor = .systemBackground
         
-        self.view.addSubview(self.tempLabel)
-        self.tempLabel.textColor = .black
-        self.tempLabel.text = "버디 커스텀 화면입니다."
-        self.view.addSubview(UILabel())
-        
-        self.tempLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.configure()
+    }
+    
+    private func configure() {
+        self.configureScrollView()
+        self.configureContentView()
+        self.configureNameTextField()
+        self.configureLineView()
+        self.configureMyBuddyImageView()
+        self.configureColorTitleLabel()
+        self.configureColorCollectionView()
+        self.configureFaceTitleLabel()
+        self.configureFaceCollectionView()
+    }
+    
+    private func configureScrollView() {
+        self.view.addSubview(self.scrollView)
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            self.tempLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.tempLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            self.scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            self.scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
         ])
+    }
+    
+    private func configureContentView() {
+        self.scrollView.addSubview(self.contentView)
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.contentView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
+            self.contentView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
+            self.contentView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor),
+            self.contentView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor),
+            self.contentView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor)
+        ])
+    }
+    
+    private func configureNameTextField() {
+        self.contentView.addSubview(self.nameTextField)
+        self.nameTextField.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configureLineView() {
+        self.contentView.addSubview(self.lineView)
+        self.lineView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configureMyBuddyImageView() {
+        self.contentView.addSubview(self.myBuddyImageView)
+        self.myBuddyImageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configureColorTitleLabel() {
+        self.contentView.addSubview(self.colorTitleLabel)
+        self.colorTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configureColorCollectionView() {
+        self.contentView.addSubview(self.colorCollectionView)
+        self.colorCollectionView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configureFaceTitleLabel() {
+        self.contentView.addSubview(self.faceTitleLabel)
+        self.faceTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configureFaceCollectionView() {
+        self.contentView.addSubview(self.faceCollectionView)
+        self.faceCollectionView.translatesAutoresizingMaskIntoConstraints = false
     }
     
 }

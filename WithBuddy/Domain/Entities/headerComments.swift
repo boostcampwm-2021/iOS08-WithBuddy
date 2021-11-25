7fa4798b19b6037b76d2e689245d1d588050569b
dailynj <leena9901@gmail.com>
2021-11-25 14:21:54 +0900

    (#194) feat: 상단 문구 enum 생성

---
+++ WithBuddy/Domain/Entities/headerComments.swift
@@ -0,0 +1,21 @@
+//
+//  headerComments.swift
+//  WithBuddy
+//
+//  Created by 이나정 on 2021/11/25.
+//
+
+import Foundation
+
+enum HeaderComments: String {
+    
+    case gatheringToday = "오늘 모임은 재미있으셨나요?\n오늘 모임에 메모와 사진을 기록해보아요!"
+    case noGathering7 = "7일간 약속이 없어요.\n버디와 새로운 약속을 만들어보아요!"
+    case noGathering6 = "6일간 약속이 없어요.\n버디와 새로운 약속을 만들어보아요!"
+    case noGathering5 = "5일간 약속이 없어요.\n버디와 새로운 약속을 만들어보아요!"
+    case noGathering4 = "4일간 약속이 없어요.\n버디와 새로운 약속을 만들어보아요!"
+    case fullGathering5 = "5일간 모임이 있네요! 위드버디에서 정확한 시간과 장소를 확인해봐요!"
+    case fullGathering4 = "4일간 모임이 있네요! 위드버디에서 정확한 시간과 장소를 확인해봐요!"
+    case fullGathering3 = "3일간 모임이 있네요! 위드버디에서 정확한 시간과 장소를 확인해봐요!"
+    
+}

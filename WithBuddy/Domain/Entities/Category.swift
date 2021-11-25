c4b782b8fc58d2ce9a22ddf5dd7a61998c4e4b10
duyeonnn <dykim0828@yonsei.ac.kr>
2021-11-25 14:11:24 +0900

    (#207) refactor: PlaceType을 Category 이름으로 변경

---
+++ WithBuddy/Domain/Entities/Category.swift
@@ -0,0 +1,55 @@
+//
+//  PlaceType.swift
+//  WithBuddy
+//
+//  Created by 이나정 on 2021/11/02.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+     
+    case study = "Study"
+    case shopping = "Shopping"
+    case sport = "Sport"
+    case healing = "Healing"
+    case hobby = "Hobby"
+    case culture = "Culture"
+    case extracurricularActivities = "ExtraCurricularActivities"
+    case meal = "Meal"
+    case etc = "Etc"
+    
+}
+
+extension Category: CustomStringConvertible {
+    
+    var description: String {
+        return self.rawValue
+    }
+    
+}
+
+extension Category: Comparable {
+    
+    static func < (lhs: Category, rhs: Category) -> Bool {
+        return lhs.order < rhs.order
+    }
+    
+    
+    private var order: Int {
+        switch self {
+        case .study: return 0
+        case .shopping : return 1
+        case .sport: return 2
+        case .healing: return 3
+        case .hobby: return 4
+        case .culture: return 5
+        case .extracurricularActivities: return 6
+        case .meal: return 7
+        case .etc: return 8
+        }
+    }
+    
+    
+    
+}

6184598ede4fb4aefb2cc4594240be7be50819e0
duyeonnn <dykim0828@yonsei.ac.kr>
2021-12-01 02:03:51 +0900

    (#281) feat: UserCustomViewController 생성

---
+++ WithBuddy/Presentation/UserCustom/View/UserCustomViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserCustomViewController.swift
+//  WithBuddy
+//
+//  Created by 김두연 on 2021/12/01.
+//
+
+import UIKit
+
+class UserCustomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
